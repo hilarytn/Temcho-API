39fe851e272dad42f9c8ce84f5f5d97fbf358d92
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-18 14:50:19 +0000

    Add isDeleted to User model for soft delete

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["admin", "user"], default: "user" },
+  isDeleted: { type: Boolean, default: false }
 }, { timestamps: true });
 
 export default mongoose.model("User", UserSchema);
