02fcc9a36a8403f01f9ac360ce67f1225490cb2f
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-18 13:33:47 +0000

    add cors and json parsing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import cors from "cors";
 import authRoutes from './routes/authRoutes.js'
 import adminRoutes from './routes/adminRoutes.js'
 
@@ -13,9 +14,12 @@ connectDB()
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors);
+app.use(express.json());
+
 // // Routes
-app.use('/api/users', authRoutes);
-app.use("/api/admin", adminRoutes);
+app.use('/api/v1/users', authRoutes);
+app.use("/api/v1/admin", adminRoutes);
 
 app.listen(PORT, ()=> {
     console.log(`Server running on PORT ${PORT}`.underline.yellow)
