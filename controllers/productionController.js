163abfdfb5a0a3291aba4799fe095e500401374f
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-19 17:21:26 +0000

    Fix spelling to populate operator, not operators

--- controllers/productionController.js
+++ controllers/productionController.js
@@ -35,8 +35,8 @@ export const createProductionRecord = async (req, res) => {
   
   export const updateProductionRecord = async (req, res) => {
     try {
-      const { quantity, date, shift, operators, nylonRollsUsed, packingBagsUsed, powerSources } = req.body;
-      const record = await Production.findByIdAndUpdate(req.params.id, { quantity, date, shift, operator, nylonRollsUsed, packingBagsUsed, powerSources }, { new: true }).populate("operators");
+      const { quantity, date, shift, operator, nylonRollsUsed, packingBagsUsed, powerSources } = req.body;
+      const record = await Production.findByIdAndUpdate(req.params.id, { quantity, date, shift, operator, nylonRollsUsed, packingBagsUsed, powerSources }, { new: true }).populate("operator");
       res.status(200).json({ message: "Production record updated", record });
     } catch (error) {
       res.status(500).json({ message: "Server error", error });
