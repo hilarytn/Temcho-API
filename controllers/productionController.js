0505c8b8d1a145782fa9995060c99b7ec5e78c59
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-19 16:11:37 +0000

    Adjust operator to be multiple

--- controllers/productionController.js
+++ controllers/productionController.js
@@ -1,26 +1,13 @@
-
-
 export const createProductionRecord = async (req, res) => {
     try {
-      const { quantity, date, shift, operator, nylonRollsUsed, packingBagsUsed, powerSource } = req.body;
-  
-      const productionRecord = await Production.create({
-        quantity,
-        date,
-        shift,
-        operator,
-        nylonRollsUsed,
-        packingBagsUsed,
-        powerSource,
-      });
-  
+      const { quantity, date, shift, operator, nylonRollsUsed, packingBagsUsed, powerSources } = req.body;
+      const productionRecord = await Production.create({ quantity, date, shift, operator: Array.isArray(operator) ? operator : [operator], nylonRollsUsed, packingBagsUsed, powerSources });
       res.status(201).json({ message: "Production record created", productionRecord });
     } catch (error) {
       res.status(500).json({ message: "Server error", error });
     }
   };
   
-
   export const getAllProductionRecords = async (req, res) => {
     try {
       const records = await Production.find({ isDeleted: false });
@@ -45,7 +32,7 @@ export const createProductionRecord = async (req, res) => {
   export const updateProductionRecord = async (req, res) => {
     try {
       const { quantity, date, shift, operator, nylonRollsUsed, packingBagsUsed, powerSources } = req.body;
-      const record = await Production.findByIdAndUpdate(req.params.id, { quantity, date, shift, operator, nylonRollsUsed, packingBagsUsed, powerSources }, { new: true });
+      const record = await Production.findByIdAndUpdate(req.params.id, { quantity, date, shift, operator: Array.isArray(operator) ? operator : [operator], nylonRollsUsed, packingBagsUsed, powerSources }, { new: true });
       res.status(200).json({ message: "Production record updated", record });
     } catch (error) {
       res.status(500).json({ message: "Server error", error });
@@ -65,4 +52,3 @@ export const createProductionRecord = async (req, res) => {
       res.status(500).json({ message: "Server error", error });
     }
   };
-  
