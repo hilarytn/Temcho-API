55f4973bc0d7a2c69f423fd823515bad6dc6c1d1
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-19 16:23:26 +0000

    Reference Operator model

--- models/Production.js
+++ models/Production.js
@@ -5,10 +5,10 @@ const productionSchema = new mongoose.Schema(
     quantity: { type: Number, required: true },
     date: { type: Date, required: true },
     shift: { type: [String], enum: ["morning", "afternoon", "night"], required: true },
-    operator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    nylonRollsUsed: { type: Number, required: true }, // Track rolls of nylon used
-    packingBagsUsed: { type: Number, required: true }, // Track number of packing bags used
-    powerSource: { type: [String], enum: ["mains", "diesel"], required: true }, // Track power source
+    operator: [{ type: mongoose.Schema.Types.ObjectId, ref: "Operator", required: false }],
+    nylonRollsUsed: { type: Number, required: true },
+    packingBagsUsed: { type: Number, required: true },
+    powerSource: { type: [String], enum: ["mains", "diesel"], required: true },
     isDeleted: { type: Boolean, default: false },
   },
   { timestamps: true }
