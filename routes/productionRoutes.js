b92f83aa688dc50168dac469a54eb12ad4e6e05a
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-19 16:08:15 +0000

    Adjust production routes endpoint

--- routes/productionRoutes.js
+++ routes/productionRoutes.js
@@ -11,10 +11,10 @@ import { authMiddleware, isAdmin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 // Routes for production records
-router.post("/production", authMiddleware, createProductionRecord);
-router.get("/production", authMiddleware, getAllProductionRecords);
-router.get("/production/:id", authMiddleware, getProductionRecordById);
-router.put("/production/:id", authMiddleware, isAdmin, updateProductionRecord);
-router.delete("/production/:id", authMiddleware, isAdmin, deleteProductionRecord);
+router.post("/create", authMiddleware, createProductionRecord);
+router.get("/all", authMiddleware, getAllProductionRecords);
+router.get("/:id", authMiddleware, getProductionRecordById);
+router.put("/:id", authMiddleware, isAdmin, updateProductionRecord);
+router.delete("/:id", authMiddleware, isAdmin, deleteProductionRecord);
 
 export default router;
