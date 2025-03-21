d48535da7fbdc2afaf0f0e31eaf44dd30ccb8c42
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-21 12:20:11 +0000

    Add pagination to get inventory, implement get inventory by id

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,4 +1,5 @@
-// Inventory Controllers
+import Inventory from '../models/Inventory.js'
+
 export const addInventory = async (req, res) => {
     try {
       const { itemName, quantity, unit } = req.body;
@@ -12,16 +13,36 @@ export const addInventory = async (req, res) => {
     }
   };
   
-export const getInventory = async (req, res) => {
+  export const getInventory = async (req, res) => {
     try {
-      const inventory = await Inventory.find().populate("addedBy", "name email");
-      res.status(200).json(inventory);
+      const { page = 1, limit = 10 } = req.query;
+      const inventory = await Inventory.find()
+        .skip((page - 1) * limit)
+        .limit(parseInt(limit));
+      const total = await Inventory.countDocuments();
+  
+      res.status(200).json({
+        total,
+        page: parseInt(page),
+        limit: parseInt(limit),
+        inventory,
+      });
     } catch (error) {
       res.status(500).json({ message: "Server error", error: error.message });
     }
-};
+  };
   
-export const updateInventory = async (req, res) => {
+  export const getInventoryById = async (req, res) => {
+    try {
+      const inventoryItem = await Inventory.findById(req.params.id);
+      if (!inventoryItem) return res.status(404).json({ message: "Inventory item not found" });
+      res.status(200).json(inventoryItem);
+    } catch (error) {
+      res.status(500).json({ message: "Server error", error: error.message });
+    }
+  };
+  
+  export const updateInventory = async (req, res) => {
     try {
       const updatedItem = await Inventory.findByIdAndUpdate(req.params.id, req.body, { new: true });
       if (!updatedItem) return res.status(404).json({ message: "Inventory item not found" });
@@ -29,14 +50,13 @@ export const updateInventory = async (req, res) => {
     } catch (error) {
       res.status(500).json({ message: "Server error", error: error.message });
     }
-};
+  };
   
-export const deleteInventory = async (req, res) => {
+  export const deleteInventory = async (req, res) => {
     try {
       await Inventory.findByIdAndDelete(req.params.id);
       res.status(200).json({ message: "Inventory item deleted successfully" });
     } catch (error) {
       res.status(500).json({ message: "Server error", error: error.message });
     }
-};
-  
+  };
