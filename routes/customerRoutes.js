d632ebdb7fca0c9f0916360618bf358622d99471
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-29 15:38:20 +0000

    Register getCustomerById-route and controller

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createCustomer, getCustomers, updateCustomer, deleteCustomer } from "../controllers/customerController.js";
+import { createCustomer, getCustomers, updateCustomer, deleteCustomer, getCustomerById } from "../controllers/customerController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/create", authMiddleware, createCustomer);
+router.get("/:id", authMiddleware, getCustomerById);
 router.get("/", authMiddleware, getCustomers);
 router.put("/:id", authMiddleware, updateCustomer);
 router.delete("/:id", authMiddleware, deleteCustomer);
