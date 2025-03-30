942ef0c8c90eeda56c68bca0dad04533ac56c687
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2025-03-30 12:31:44 +0000

    Fix login route

--- public/js/login.js
+++ public/js/login.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
       const password = $('#password').val();
       
       try {
-        const response = await fetch('/api/auth/login', {
+        const response = await fetch('/api/v1/users/login', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ email, password })
@@ -28,4 +28,3 @@ $(document).ready(function () {
       }
     });
   });
-  
